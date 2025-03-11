be0a70e218ee0f622f1a916d59440473256dc177
Clomby <clomby@metrix.pw>
2025-03-11 12:59:57 +0100

    Fix error logging format to include client information

--- events/error.js
+++ events/error.js
@@ -1,7 +1,7 @@
 const logger = require("../modules/Logger.js");
 module.exports = async (client, error) => {
   logger.log(
-    `An error event was sent by Discord.js: \n${JSON.stringify(error)}`,
+    `An error event was sent by Discord.js: \n${client + error}`,
     "error"
   );
 };
